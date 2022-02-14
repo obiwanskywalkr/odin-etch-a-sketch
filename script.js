70c8c1551abeae90ac36bd653d1ff611479d6419
obiwanskywalkr <gp47spd13@gmail.com>
2022-02-14 13:14:04 -0600

    Add resize feature

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
 const container = document.querySelector('#container');
+const slider = document.querySelector('#size-slider');
+let size = (512 / slider.value);
 
-for (let i = 0; i < 256; i++) {
-    const grid = document.createElement('div');
-    container.appendChild(grid);
+slider.addEventListener('input', () => {
+    updateSize();
+});
+slider.addEventListener('mousedown', () => {
+    clearGrid();
+})
+slider.addEventListener('mouseup', () => {
+    newGrid();
+});
+
+window.onload = newGrid();
+
+function newGrid() {
+    for (let i = 0; i < (slider.value * slider.value); i++) {
+        let box = document.createElement('div');
+        box.setAttribute('class', 'box');
+        box.style.width = `${size}px`;
+        box.style.height = `${size}px`;
+        box.addEventListener('mouseover', (e) => {
+            e.currentTarget.classList.add('colored');
+        })
+        container.appendChild(box);
+    }
+}
+function newColorGrid() {
+    for (let i = 0; i < (slider.value * slider.value); i++) {
+        let box = document.createElement('div');
+        box.setAttribute('class', 'box');
+        box.style.width = `${size}px`;
+        box.style.height = `${size}px`;
+        box.addEventListener('mouseover', (e) => {
+            e.currentTarget.classList.add('.colored');
+        });
+        container.appendChild(box);
+    }
 }
 
-const box = document.querySelectorAll('div>div')
+function clearGrid() {
+    let boxes = document.querySelectorAll('.box');
+    for(let i = 0; i < (boxes.length); i++) {
+        container.removeChild(boxes[i]);
+    } 
+}
 
-box.forEach((div) => {
-    div.addEventListener('mouseover', (e) => {
-        e.currentTarget.classList.add('colored')
-    });
+function updateSize() {
+    const sizeContainer = document.querySelector('#size-value');
+    sizeContainer.textContent = `${slider.value} x ${slider.value}`;
+    size = (512 / slider.value);
+}
+
+const trash = document.querySelector('#trash');
+trash.addEventListener('click', () => {
+    clearGrid();
+    newGrid();
 });
